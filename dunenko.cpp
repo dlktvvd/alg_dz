ac0cee17c9ccd9842e0cd8acae25b9a8e06d7d51
fgg-gg <alpuganova2016@gmail.com>
2025-12-17 20:39:10 +0300

    испр

--- dunenko.cpp
+++ dunenko.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-int main() {
-    int n, amount;
-    cin >> n >> amount;
-
-    vector<int> coins(n);
-    for (int i = 0; i < n; i++) {
-        cin >> coins[i];
-    }
+int bfsCoinChange(vector<int>& coins, int amount) {
+    if (amount < 0) return -1;
+    if (amount == 0) return 0;
 
     vector<int> d(amount + 1, -1);
     queue<int> q;
@@ -23,8 +17,8 @@ int main() {
         int x = q.front();
         q.pop();
 
-        for (int i = 0; i < n; i++) {
-            int y = x + coins[i];
+        for (int coin : coins) {
+            int y = x + coin;
             if (y <= amount && d[y] == -1) {
                 d[y] = d[x] + 1;
                 q.push(y);
@@ -32,7 +26,5 @@ int main() {
         }
     }
 
-    cout << d[amount] << endl;
-
-    return 0;
+    return d[amount];
 }
